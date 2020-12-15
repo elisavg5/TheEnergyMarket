ec14b917c2a3b6a94e1d2571bfbe31a8d6048dc4
elisavg5 <71868956+elisavg5@users.noreply.github.com>
2020-12-15 11:53:04 +0100

    Add files via upload

---
+++ economics.py
@@ -0,0 +1,21 @@
+
+from multiprocessing import Process
+import os
+import time 
+import threading
+import math
+
+def Economics():
+
+    ecoPb = False #boucle qui lance un problème éco aléatoirement 
+    while eco == false:
+        proba = math.random()
+        if proba > 0.90:
+            ecoPb = True
+    typePb = int(math.random()*2)+1 #1 : offre qui augmente, prix qui baisse #2 : offre qui diminue, prix qui augmente
+    if typePb == 1:
+        #fonction de market qui augmente le prix
+    else:
+        #fonction de market qui augmente le prix
+
+
