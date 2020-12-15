d584ed326db8ffec0c8f3cbe253f530ff8e78129
juliarougier <72855563+juliarougier@users.noreply.github.com>
2020-12-15 11:50:52 +0100

    Add files via upload

---
+++ politics.py
@@ -0,0 +1,34 @@
+from multiprocessing import Process
+import os
+import time 
+import threading
+import math
+import system 
+
+def politics(polProblem)
+
+    polProblem bool = false
+
+    while polProblem == false : 
+        proba = float(math.random())
+        if proba >= 0,90 :
+            polProblem == true
+            
+    
+    typeProbPol = int(math.random()*2)+1
+    if typeProbPol == 1 : #Embargo 
+        #Fonction de market qui restreint la demande énergétique
+    elif typeProbPol == 2 : #War
+        #Fonction de market qui restreint la demande énergétique 
+     
+
+    
+
+
+
+
+
+
+
+
+
